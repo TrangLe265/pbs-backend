e94531da565352964bd2a9c0e9f7383d61900725
Trang Le <trang.le@myy.haaga-helia.fi>
2025-07-05 12:27:57 +0300

    correct the coefficient

--- sql/mirgations/create_dots_coefficients_table.sql
+++ sql/mirgations/create_dots_coefficients_table.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS dots_coefficients (
 );
 
 INSERT INTO dots_coefficients (sex, a, b, c, d, e) VALUES
-    ('male', 0.000001093, 0.0007391293, 0.1918759221, 24.0900756, 307.75076),
-    ('female', -0.0000010706, 0.0005158568, 0.1126655495, 13.6175032, 57.96288);
+    ('male', -0.000001093, 0.0007391293, -0.1918759221, 24.0900756, -307.75076),
+    ('female', -0.0000010706, 0.0005158568, -0.1126655495, 13.6175032, -57.96288);
 
 /*SERIAL id: suitable if the data is set with certain values*/
