ec011af3d8debfe4ddf7f9ce7045203a64647299
Trang Le <trang.le@myy.haaga-helia.fi>
2025-10-03 16:41:45 +0300

    add docker to the project

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ const { Pool } = require('pg'); // Import the Pool class from the 'pg' library t
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: String(process.env.DB_PASSWORD),
-    port: parseInt(process.env.DB_PORT),
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST ,
+  port: process.env.DB_PORT ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
 
 pool.query('SELECT NOW()', (err, res) => {
     if (err) {
