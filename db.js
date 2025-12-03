8aaf5bf7178c554e43a5318d125ebd0064dfd2c1
TrangLe265 <trang.le@myy.haaga-helia.fi>
2025-12-03 11:22:14 +0200

    remove the unneccesary db connection tetsing code

--- db.js
+++ db.js
@@ -9,15 +9,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
 });
 
-
-pool.query('SELECT NOW()', (err, res) => {
-    if (err) {
-        console.error('Database connection error:', err);
-    } else {
-        console.log('Database connected successfully:', res.rows[0]);
-    }
-});
-
 module.exports = {
     query: (text, params) => pool.query(text, params)
 };
