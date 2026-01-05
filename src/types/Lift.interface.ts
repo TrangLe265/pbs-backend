ad78c7366e41d7bd2ed3510d2803b036672978a1
TrangLe265 <trang.le@myy.haaga-helia.fi>
2026-01-05 15:34:30 +0700

    added some DTO interface

--- src/types/Lift.interface.ts
+++ src/types/Lift.interface.ts
@@ -1,8 +1,31 @@
 export interface Lift{
     id: number;
-    userId: number;
+    user_id: number;
     weight_lifted: number; 
     lift_type_id: number;
     date: Date;
+    notes?: string | ''
+}
+
+export interface LiftIdParam {
+    id: string,
+}
+
+export interface AllLiftByTypeParam {
+    user_id: string;
+    lift_type_id: string; 
+}
+
+export interface NewLift {
+    user_id: number;
+    weight_lifted: number; 
+    lift_type_id: number;
+    date: Date;
+    notes?: string 
+}
+
+export interface EditLift {
+    weight_lifted: number;
+    date?: Date;
     notes?: string
 }
